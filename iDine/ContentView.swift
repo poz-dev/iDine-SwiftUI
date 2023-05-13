567a58f643d101fe102783e9cb289ed5c0278fee
Kresimir Ivanjko <tp8pbrzyr4@privaterelay.appleid.com>
2023-05-13 22:36:20 +0200

    1

--- iDine/ContentView.swift
+++ iDine/ContentView.swift
@@ -8,14 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    let  menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(menu) { section in
+                    Section(section.name) {
+                        ForEach(section.items) { item in
+                            ItemRow(item: item)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Menu")
+            .listStyle(.grouped)
         }
-        .padding()
     }
 }
 
