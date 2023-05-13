8291d0dcf8d087995a306e37f2fd06c1657a27f2
Kresimir Ivanjko <tp8pbrzyr4@privaterelay.appleid.com>
2023-05-13 21:52:03 +0200

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Kresimir Ivanjko on 13.05.2023..
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
